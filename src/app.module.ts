1b271a6770b80f1e58cd2fdb250aa90d11d525d6
Hesham127 <hmahmoud.hm2004@gmail.com>
2026-08-29 15:26:43 +0300

    Initialize_Modules

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { OrganizationModule } from './organization/organization.module.js';
+import { WorkModule } from './work/work.module.js';
+import { CollaborationModule } from './collaboration/collaboration.module.js';
+import { IntegrationsModule } from './integrations/integrations.module.js';
+import { MeetingsModule } from './meetings/meetings.module.js';
+
+@Module({
+  imports: [OrganizationModule, WorkModule, CollaborationModule, IntegrationsModule, MeetingsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
