3668873e9d3538ac045cd5b87bfe3026b9bbb771
Hesham127 <hmahmoud.hm2004@gmail.com>
2026-09-03 07:06:34 +0300

    Initialize first schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { OrganizationModule } from './organization/organization.module.js';
 import { WorkModule } from './work/work.module.js';
-import { CollaborationModule } from './collaboration/collaboration.module.js';
 import { IntegrationsModule } from './integrations/integrations.module.js';
 import { MeetingsModule } from './meetings/meetings.module.js';
 
 @Module({
-  imports: [OrganizationModule, WorkModule, CollaborationModule, IntegrationsModule, MeetingsModule],
+  imports: [OrganizationModule, WorkModule, IntegrationsModule, MeetingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
